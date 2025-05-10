97cd7ef21408e99f297a29ca5b86d14bb5a25d2a
goratsy <gorpetros06@gmail.com>
2025-05-10 14:09:40 +0300

    Refactor: for smart contract hardhat config remove url blockchain in env file

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     development: {
-      url: "http://localhost:7545",
+      url: process.env.URL_GANACHE,
       chainId: Number(process.env.CHAIN_ID),
     }
   }
